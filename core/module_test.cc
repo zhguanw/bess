b129771e0c43cd3bd84ed15bea37bc6c5ded667b
Shinae Woo <shinae2012@gmail.com>
2017-11-29 15:00:34 -0800

    Thread-safe iterative scheduler
    - Move PacketBatch from gate to per-worker (per-thread) as gate are
    shared by multiple threads

--- core/module_test.cc
+++ core/module_test.cc
@@ -381,5 +381,12 @@ TEST_F(ModuleTester, SetIGatePriority) {
   EXPECT_EQ(5, m6->igates()[0]->priority());
   EXPECT_EQ(2, m7->igates()[0]->priority());
   EXPECT_EQ(2, m8->igates()[0]->priority());
+
+  EXPECT_EQ(1 + 2, m1->igates()[0]->g_idx() + m2->igates()[0]->g_idx());
+  EXPECT_EQ(3 + 4 + 5, m3->igates()[0]->g_idx() + m7->igates()[0]->g_idx() +
+                           m8->igates()[0]->g_idx());
+  EXPECT_EQ(6, m4->igates()[0]->g_idx());
+  EXPECT_EQ(7, m5->igates()[0]->g_idx());
+  EXPECT_EQ(8, m6->igates()[0]->g_idx());
 }
 }  // namespace
